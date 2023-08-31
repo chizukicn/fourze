f9e98343b46afaf0bdd5391bb20cca3f9996fd6f
chizukicn <chizukicn@outlook.com>
2023-09-01 00:39:44 +0800

    chore: up deps

--- playgrounds/vite/src/app.vue
+++ playgrounds/vite/src/app.vue
@@ -1,146 +1,146 @@
 <script setup lang="tsx">
-  import { getGlobalMockApp } from "@fourze/mock";
-  import { useAsyncState } from "@vueuse/core";
-  import axios from "axios";
-  import dayjs from "dayjs";
-  import $ from "jquery";
-  import querystring from "query-string";
-  import { computed, reactive, ref, watch } from "vue";
-  import { HiItem, HiSelection } from "hoci";
-  import Loading from "./components/base/loading.vue";
-  import Table from "./components/base/table.vue";
-  import type { TableColumns } from "./components/hooks/table";
-  import HiButton from "@/components/base/button.vue";
+import { getGlobalMockApp } from "@fourze/mock";
+import { useAsyncState } from "@vueuse/core";
+import axios from "axios";
+import dayjs from "dayjs";
+import $ from "jquery";
+import querystring from "query-string";
+import { computed, reactive, ref, watch } from "vue";
+import { HiItem, HiSelection } from "hoci";
+import Loading from "./components/base/loading.vue";
+import Table from "./components/base/table.vue";
+import type { TableColumns } from "./components/hooks/table";
+import HiButton from "@/components/base/button.vue";
 
-  const _mockEnabled = ref(!!getGlobalMockApp()?.enabled);
+const _mockEnabled = ref(!!getGlobalMockApp()?.enabled);
 
-  const mockEnabled = computed({
-    get() {
-      return _mockEnabled.value;
-    },
-    set(value) {
-      const router = getGlobalMockApp();
-      if (router) {
-        if (value) {
-          router.enable();
-        } else {
-          router.disable();
-        }
+const mockEnabled = computed({
+  get() {
+    return _mockEnabled.value;
+  },
+  set(value) {
+    const router = getGlobalMockApp();
+    if (router) {
+      if (value) {
+        router.enable();
+      } else {
+        router.disable();
       }
-      _mockEnabled.value = value;
     }
-  });
+    _mockEnabled.value = value;
+  }
+});
 
-  const args = reactive({
-    type: "fetch" as "fetch" | "axios" | "jquery",
-    keyword: "",
-    page: 1
-  });
+const args = reactive({
+  type: "fetch" as "fetch" | "axios" | "jquery",
+  keyword: "",
+  page: 1
+});
 
-  const columns: TableColumns<UserInfo> = [
-    {
-      dataIndex: "username",
-      title: "User Name",
-      width: 160
-    },
-    {
-      dataIndex: "phone",
-      title: "Phone",
-      width: 160
-    },
-    {
-      dataIndex: "createdTime",
-      title: "Created Time",
-      width: 160,
-      render({ record }) {
-        return dayjs(record.createdTime).format("YYYY-MM-DD HH:mm:ss");
-      }
-    },
-    {
-      dataIndex: "source",
-      title: "Source",
-      width: 160
-    },
-    {
-      dataIndex: "operation",
-      title: "Operation",
-      width: 160,
-      render({ record }) {
-        return <div class="space-x-2">
+const columns: TableColumns<UserInfo> = [
+  {
+    dataIndex: "username",
+    title: "User Name",
+    width: 160
+  },
+  {
+    dataIndex: "phone",
+    title: "Phone",
+    width: 160
+  },
+  {
+    dataIndex: "createdTime",
+    title: "Created Time",
+    width: 160,
+    render({ record }) {
+      return dayjs(record.createdTime).format("YYYY-MM-DD HH:mm:ss");
+    }
+  },
+  {
+    dataIndex: "source",
+    title: "Source",
+    width: 160
+  },
+  {
+    dataIndex: "operation",
+    title: "Operation",
+    width: 160,
+    render({ record }) {
+      return <div class="space-x-2">
         <HiButton size="small">Edit</HiButton>
         <HiButton size="small" class="!bg-red-400" onClick={() => deleteById(record.id)}>Delete</HiButton>
       </div>;
-      }
-    }
-  ];
-
-  interface RequestOptions {
-    url: string
-    method?: string
-    params?: Record<string, any>
-    type?: "jquery" | "fetch" | "axios"
-    data?: Record<string, any>
-  }
-
-  async function request(options: RequestOptions) {
-    const { url, method = "GET", params = {}, data = {}, type = "fetch" } = options;
-    const _url = url;
-    switch (type) {
-      case "jquery":
-        return $.ajax({
-          url: querystring.stringifyUrl({
-            url: _url,
-            query: params
-          }),
-          data,
-          method
-        }).then(r => r.data);
-      case "axios":
-        return axios(_url, {
-          method,
-          params,
-          data
-        }).then(r => r.data.data);
-      case "fetch":
-      default:
-        return await fetch(querystring.stringifyUrl({ url: _url, query: params }), {
-          body: ["GET", "HEAD", "DELETE"].includes(method.toUpperCase()) ? undefined : JSON.stringify(data),
-          method
-        }).then(r => r.json()).then(r => r.data);
     }
   }
+];
 
-  const { state, isLoading, execute } = useAsyncState<PagingData<UserInfo>>(() => {
-    return request({
-      url: "/api/item/list",
-      params: {
-        page: args.page,
-        keyword: args.keyword
-      },
-      type: args.type,
-      method: "get"
-    }) as Promise<PagingData<UserInfo>>;
-  }, {
-    items: [],
-    totalCount: 0,
-    totalPageCount: 0,
-    currentPageIndex: 1,
-    pageSize: 10,
-    nextIndex: 2,
-    previousIndex: 0,
-    startIndex: 1
-  }, { resetOnExecute: false });
+interface RequestOptions {
+  url: string
+  method?: string
+  params?: Record<string, any>
+  type?: "jquery" | "fetch" | "axios"
+  data?: Record<string, any>
+}
 
-  async function deleteById(id: string) {
-    await request({
-      url: `/api/item/${id}`,
-      type: args.type,
-      method: "DELETE"
-    });
-    await execute();
+async function request(options: RequestOptions) {
+  const { url, method = "GET", params = {}, data = {}, type = "fetch" } = options;
+  const _url = url;
+  switch (type) {
+    case "jquery":
+      return $.ajax({
+        url: querystring.stringifyUrl({
+          url: _url,
+          query: params
+        }),
+        data,
+        method
+      }).then(r => r.data);
+    case "axios":
+      return axios(_url, {
+        method,
+        params,
+        data
+      }).then(r => r.data.data);
+    case "fetch":
+    default:
+      return await fetch(querystring.stringifyUrl({ url: _url, query: params }), {
+        body: ["GET", "HEAD", "DELETE"].includes(method.toUpperCase()) ? undefined : JSON.stringify(data),
+        method
+      }).then(r => r.json()).then(r => r.data);
   }
+}
 
-  watch([args, mockEnabled], () => execute(), { deep: true });
+const { state, isLoading, execute } = useAsyncState<PagingData<UserInfo>>(() => {
+  return request({
+    url: "/api/item/list",
+    params: {
+      page: args.page,
+      keyword: args.keyword
+    },
+    type: args.type,
+    method: "get"
+  }) as Promise<PagingData<UserInfo>>;
+}, {
+  items: [],
+  totalCount: 0,
+  totalPageCount: 0,
+  currentPageIndex: 1,
+  pageSize: 10,
+  nextIndex: 2,
+  previousIndex: 0,
+  startIndex: 1
+}, { resetOnExecute: false });
+
+async function deleteById(id: string) {
+  await request({
+    url: `/api/item/${id}`,
+    type: args.type,
+    method: "DELETE"
+  });
+  await execute();
+}
+
+watch([args, mockEnabled], () => execute(), { deep: true });
 </script>
 
 <template>
