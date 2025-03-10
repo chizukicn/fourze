a9286c35f0af81845b255699601b38ccc92bdd56
chizukicn <chizukicn@outlook.com>
2025-03-10 16:54:03 +0800

    chore: update

--- packages/core/src/shared/index.ts
+++ packages/core/src/shared/index.ts
@@ -5,5 +5,4 @@ export * from "./meta";
 export * from "./props";
 export * from "./request";
 export * from "./response";
-export * from "./route";
 export * from "./version";
--- packages/core/src/shared/request.ts
+++ packages/core/src/shared/request.ts
@@ -1,12 +1,11 @@
 import type { MaybeRegex } from "maybe-types";
 import type { IncomingMessage } from "node:http";
 import type { PolyfillHeaderInit } from "../polyfill";
-import type { FourzeApp } from "./interface";
+import type { FourzeApp, FourzeHandle } from "./interface";
 import type { FourzeRouteMeta } from "./meta";
 import type { DefaultData, ExtractPropTypes, ExtractPropTypesWithIn, ObjectProps } from "./props";
-import type { FourzeRoute } from "./route";
 import { safeParse } from "fast-content-type-parse";
-import { getQuery, parseQuery, parseURL, withoutBase } from "ufo";
+import { getQuery, parseQuery, parseURL, withBase, withoutBase } from "ufo";
 import { decodeFormData, flatHeaders, getHeaderValue } from "../polyfill";
 import { isString, isUint8Array, parseJson } from "../utils";
 import { validateProps, withDefaults } from "./props";
@@ -252,4 +251,99 @@ export function createRequest(options: FourzeRequestOptions) {
 
 export function isFourzeRequest(obj: any): obj is FourzeRequest {
   return !!obj && !!obj[FourzeRequestFlag];
+}
+
+const FourzeRouteFlag = "__isFourzeRoute";
+
+export interface FourzeRouteOptions<Props extends ObjectProps = ObjectProps, Meta = FourzeRouteMeta> {
+  method?: RequestMethod;
+  props?: Props;
+  meta?: Meta & FourzeRouteMeta;
+}
+
+export interface FourzeBaseRoute<
+  Result = unknown,
+  Props extends ObjectProps = DefaultData,
+  Meta = FourzeRouteMeta
+> {
+  path: string;
+  method?: RequestMethod;
+  handle: FourzeHandle<Result, Props, Meta>;
+  meta?: Meta;
+  props?: Props;
+}
+
+export interface FourzeRoute<
+  Result = unknown,
+  Props extends ObjectProps = DefaultData,
+  Meta = FourzeRouteMeta
+> extends FourzeBaseRoute<Result, Props, Meta> {
+  readonly [FourzeRouteFlag]: true;
+  readonly props: Props;
+  readonly meta: Meta;
+}
+
+export type FourzeRouteGenerator<This> = {
+  [K in RequestMethod]: {
+    <
+      Result = any,
+      Props extends ObjectProps = DefaultData,
+      Meta = FourzeRouteMeta
+    >(
+      path: string,
+      options: FourzeRouteOptions<Props, Meta>,
+      handle: FourzeHandle<Result, Props, Meta>
+    ): This;
+    <Result>(path: string, handle: FourzeHandle<Result>): This;
+  };
+};
+
+export function isRoute(route: any): route is FourzeRoute<unknown> {
+  return !!route && !!route[FourzeRouteFlag];
+}
+
+const REQUEST_PATH_REGEX = new RegExp(
+  `^(${FOURZE_METHODS.join("|")})\\s+`,
+  "i"
+);
+
+export function defineRouteProps<Props extends ObjectProps = DefaultData>(
+  props: Props
+) {
+  return props;
+}
+
+export function defineRoute<
+  Result = unknown,
+  Props extends ObjectProps = DefaultData,
+  Meta = FourzeRouteMeta
+>(
+  route: FourzeBaseRoute<Result, Props, Meta> & { base?: string }
+): FourzeRoute<Result, Props, Meta> {
+  const { handle, meta = {} as Meta, props = {} as Props, base } = route;
+  let { method, path } = route;
+
+  if (REQUEST_PATH_REGEX.test(path)) {
+    const arr = path.split(/\s+/);
+    const m = arr[0].toLowerCase() as RequestMethod;
+    if (FOURZE_METHODS.includes(m)) {
+      method = m;
+      path = arr[1].trim();
+    }
+  }
+
+  path = base ? withBase(path, base) : path;
+
+  return {
+    method,
+    path,
+    meta,
+    handle,
+    get props() {
+      return props;
+    },
+    get [FourzeRouteFlag](): true {
+      return true;
+    }
+  };
 }
